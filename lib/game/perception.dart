8784ae67f1c87754f09ab26a9a1e3e2ddbf660aa
zxt <zxt3310@163.com>
2019-08-29 18:46:55 +0800

    1

---
+++ lib/game/perception.dart
@@ -0,0 +1,16 @@
+///感知训练
+import 'package:flutter/material.dart';
+
+class PerceptionTrain extends StatefulWidget {
+  @override
+  _PerceptionTrainState createState() => _PerceptionTrainState();
+}
+
+class _PerceptionTrainState extends State<PerceptionTrain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
